9b1b0d0a207bf1eb02c77690ee0208775ee959b7
Felipe M <philmes@gmail.com>
2019-08-10 21:42:32 -0300

    packages.txt for testing

--- src/test/java/com/mobiquityinc/PackerTest.java
+++ src/test/java/com/mobiquityinc/PackerTest.java
@@ -11,7 +11,7 @@ public class PackerTest {
 
 	@Test
 	public void testPacker() throws APIException {
-		String filePath = "\\tmp\\packages_test.txt";
+		String filePath = "\\tmp\\packages.txt";
 		String res = Packer.pack(filePath);
 		String expected = "4\n" + "-\n" + "2,7\n" + "8,9\n";
 		assertEquals(expected, res);
